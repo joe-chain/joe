6cf23f3a129ce2cc56ca583704f7a109c5199c56
Reece Williams <reecepbcups@gmail.com>
2022-12-13 11:58:50 -0600

    use legacy

--- proposals/software_upgrade/script.sh
+++ proposals/software_upgrade/script.sh
@@ -6,6 +6,7 @@ joed q gov proposal $ID
 
 
 # mainnet
-joed tx gov submit-proposal ./proposals/software_upgrade/v2_prop.json --from validator --keyring-backend os --chain-id joe-1 --yes --broadcast-mode block  --node https://rpc.justjoe.app:443
-joed tx gov vote 2 yes --from validator --keyring-backend os --chain-id joe-1 --yes --broadcast-mode block --node https://rpc.justjoe.app:443
-# joed q gov votes 2
+joed tx gov submit-legacy-proposal software-upgrade v2 --title="Joe v2 software upgrade" --description="Just doing some Joegrades" --upgrade-height 350000 --from validator --keyring-backend os --chain-id joe-1 --yes --broadcast-mode block --no-validate=true --node https://rpc.justjoe.app:443
+joed tx gov vote 6 yes --from validator --keyring-backend os --chain-id joe-1 --yes --broadcast-mode block --node https://rpc.justjoe.app:443
+joed q gov proposal 6
+joed q gov votes 6
