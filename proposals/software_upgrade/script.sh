ded67371e9f48db3d01fa28fd12a38ef43c9de5e
Reece Williams <reecepbcups@gmail.com>
2022-12-13 11:01:08 -0600

    Update proposal

--- proposals/software_upgrade/script.sh
+++ proposals/software_upgrade/script.sh
@@ -3,3 +3,9 @@ joed tx gov submit-proposal ./proposals/software_upgrade/v2_prop.json --from val
 ID="1"
 joed tx gov vote $ID yes --from $KEY --keyring-backend test --chain-id joe-1 --yes --broadcast-mode block --node http://localhost:26657
 joed q gov proposal $ID
+
+
+# mainnet
+joed tx gov submit-proposal ./proposals/software_upgrade/v2_prop.json --from validator --keyring-backend os --chain-id joe-1 --yes --broadcast-mode block  --node https://rpc.justjoe.app:443
+joed tx gov vote 2 yes --from validator --keyring-backend os --chain-id joe-1 --yes --broadcast-mode block --node https://rpc.justjoe.app:443
+# joed q gov votes 2
